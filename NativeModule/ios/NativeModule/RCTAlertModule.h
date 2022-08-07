1231becd650e19a239756668ab69bfd3977e11b1
jwAndroid <cjd9408er@naver.com>
2022-08-07 20:47:14 +0900

    init

---
+++ NativeModule/ios/NativeModule/RCTAlertModule.h
@@ -0,0 +1,12 @@
+//
+//  RCTAlertModule.h
+//  NativeModule
+//
+//  Created by 최지웅 on 2022/08/07.
+//
+
+#ifndef RCTAlertModule_h
+#define RCTAlertModule_h
+
+
+#endif /* RCTAlertModule_h */
---
+++ NativeModule/ios/RCTAlertModule.m
@@ -0,0 +1,8 @@
+//
+//  RCTAlertModule.m
+//  NativeModule
+//
+//  Created by 최지웅 on 2022/08/07.
+//
+
+#import <Foundation/Foundation.h>
